de4263aecd700ddea89573595f6b06861bdf4c9c
m473 <amakm473@gmail.com>
2021-10-02 23:15:36 +0700

    membuat file index.js

---
+++ algoritma-pseudocode/index.js
@@ -0,0 +1,14 @@
+var pi = 3.14;
+var r = 7;
+var luas = pi * r * r;
+
+// console.log(luas);
+
+var p = 5;
+var l = 3;
+var t = 2;
+
+// var luas = 2*p*l+2*p*t+2*l*t;
+var luas = 2*(p*l+p*t+l*t);
+
+console.log(luas);
