25a08bd59720b5ff93151f9a5402868018c782d6
m473 <amakm473@gmail.com>
2021-10-03 11:24:43 +0700

    menambahkan file jawaban soal untk algoritma pseudocode dan variables operations

---
+++ algoritma-pseudocode/jawabanSoalAlgoritmaDanPseudocode.js
@@ -0,0 +1,35 @@
+// TASK - ALGORITMA & PSEUDOCODE
+
+/** 1 
+ * Buat algoritma : How to make Indomie
+ **/
+
+
+
+
+
+/** 2 
+ * Buat algoritma : How to cook Sirloin Steak
+ **/
+
+
+
+/** 3
+ * Buat algoritma dan pseudocode :
+ * Luas persegi panjang
+ **/
+
+
+
+
+/** 4
+ * Buat algoritma dan pseudocode :
+ * Volume bola
+ **/
+
+
+
+
+/** 5 
+ * Buat algoritma : How to add, commit, push to Github
+ **/
---
+++ algoritma-pseudocode/jawabanSoalVariablesOperations.js
@@ -0,0 +1,97 @@
+// TASK - VARIABLES, OPERATIONS, JAVASCRIPT
+
+/** 
+ * 1
+ * Buat algoritma, pseudocode, dan codingan :
+ * Keliling persegi
+*/
+// Algoritma
+
+
+
+
+// Pseudocode
+
+
+
+
+// Codingan
+var sisi = 5;
+var kelilingPersegi = 4 * sisi;
+
+console.log(kelilingPersegi);
+
+// ===================================================================== //
+
+/**
+ * 2
+ * Buat algoritma, pseudocode, dan codingan :
+ * Luas permukaan balok
+ */
+// Algoritma
+
+
+
+
+// Pseudocode
+
+
+
+
+// Kodingan
+var p = 3;
+var l = 6;
+var t = 9;
+
+var luasPermukaanBalok = 2*(p*l+p*t+l*t);
+
+console.log(luasPermukaanBalok);
+
+// ===================================================================== //
+
+/**
+ * 3
+ * Buat kodingan, untuk menjawab pertanyaan " Who are you ?" menggunakan variable.
+ * 
+ *      ```javascript
+ *          //Code here
+ *          var name;
+ *          var age;
+ * 
+ *          //Output
+ *          //My name is Vincent, and i'm 28 years old.
+ *      ```
+ */
+var question = "Who are You ? ";
+var nama = "Vincent";
+var age = "28";
+
+console.log(question);
+console.log("My name is "+nama+", and i'm "+age+" years old");
+
+// ===================================================================== //
+
+/**
+ * 4
+ * Buat kodingan, untuk membuat random number. Range number 1 - 10.
+ * 
+ *      ```javascript
+ *          //Code here
+ *          var number;
+ * 
+ *          //Output 
+ *          //Your number is 1
+ *          //Your number is 5
+ *      ```
+ */
+
+
+/**
+ * 5
+ * Buat kodingan, untuk membuat Nomor Induk mahasiswa.
+ * Rumus : <Nama_Mahasiswa><Nomor_Absen><Lulus/Tidak>
+ * Contoh : James15true
+ * dengan syarat nomor absen random,  Range 1 - 50
+ */
+
+
